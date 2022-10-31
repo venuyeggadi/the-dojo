112c67ffc218b7396fd0622b3ae3294d5d8b070d
Venu Yeggadi <venu.yeggadi@thoughtworks.com>
2022-10-31 11:27:05 +0530

    Add signup page template to collect email, password, display name and profile picture

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,12 +1,81 @@
+import { useState } from 'react'
+
 // styles
-import './Signup.css';
+import './Signup.css'
 
-const Signup = () => {
+export default function Signup() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [displayName, setDisplayName] = useState('')
+  const [thumbnail, setThumbnail] = useState(null)
+  const [thumbnailError, setThumbnailError] = useState(null)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(email, password, displayName, thumbnail);
+  }
+
+  const handleFileChange = (e) => {
+    setThumbnail(null);
+    let selected = e.target.files[0];
+
+    if (!selected) {
+      setThumbnailError('Please select a file');
+      return;
+    }
+    if (!selected.type.includes('image')) {
+      setThumbnailError('Selected file must be an image');
+      return;
+    }
+    if (selected.size > 100000) {
+      setThumbnailError('Image file size must be less than 100KB')
+      return;
+    }
+
+    setThumbnailError(null);
+    setThumbnail(selected)
+  }
+
   return (
-    <div>
-      Signup
-    </div>
-  );
+    <form className="auth-form" onSubmit={handleSubmit}>
+      <h2>Sign Up</h2>
+      <label>
+        <span>Email:</span>
+        <input
+          required
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          required
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <label>
+        <span>Display Name:</span>
+        <input
+          required
+          type="text"
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+      <label>
+        <span>Profile Thumbnail:</span>
+        <input
+          required
+          type="file"
+          onChange={handleFileChange}
+        />
+        {thumbnailError && <div className='error'>{thumbnailError}</div>}
+      </label>
+      <button className="btn">Sign Up</button>
+    </form>
+  )
 }
-
-export default Signup
