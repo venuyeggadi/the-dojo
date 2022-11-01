1025140f10e5ed54f29bf8ec71994f7f88dd4370
Venu Yeggadi <venu.yeggadi@thoughtworks.com>
2022-11-01 21:49:41 +0530

    Show all the projects in dashboard

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,10 +1,10 @@
 // styles
 import './Avatar.css'
 
-export default function Avatar({ src }) {
+export default function Avatar({ src, title }) {
   return (
     <div className="avatar">
-      <img src={src} alt="user avatar" />
+      <img src={src} alt="user avatar" title={title}/>
     </div>
   )
 }
