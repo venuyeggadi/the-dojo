cca63b2543ff5f5c7891133ac31c42159e54591a
Venu Yeggadi <venu.yeggadi@thoughtworks.com>
2022-11-01 10:05:06 +0530

    Show user thumbnail and display name on sidebar

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
     <div className="App">
       {authIsReady && (
         <BrowserRouter basename={process.env.PUBLIC_URL} >
-          <Sidebar />
+          {user && <Sidebar />}
           <div className="container">
             <Navbar />
             <Switch>
